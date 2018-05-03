d6cd1f9916c577395b5375c0937fa8f7d7081a27
Tomasmed18 <tomy-medina@hotmail.com>
2018-05-03 14:51:33 -0300

    Add files via upload

--- src/tp_especial/Indicadores.java
+++ src/tp_especial/Indicadores.java
@@ -14,9 +14,35 @@ public class Indicadores {
 		float media = Indicadores.media(valores);
 		float sumatoria = 0;
 		for (int i = 0; i < valores.length; i++){
-			sumatoria += Math.pow((i-media), 2); // (x[i] - <X>)^2
+			sumatoria += Math.pow((valores[i]-media), 2); // (x[i] - <X>)^2
+			
 		}
 		sumatoria = sumatoria / (valores.length - 1); // (sumatoria / (N - 1))
 		return (float) Math.sqrt(sumatoria);
 	}
+	
+	public static float correlacionCruzada(int[] valores1, int[] valores2){
+		float sumatoria = 0f;
+		for (int i = 0; i < valores1.length; i++)
+			sumatoria += (float) valores1[i] * (float) valores2[i];
+		return (float)sumatoria / valores1.length;
+	}
+	
+	public static float covarianzaCruzada(int[] valores1, int[] valores2){
+		float correlacion = Indicadores.correlacionCruzada(valores1, valores2);
+		float media1 = Indicadores.media(valores1);
+		float media2 = Indicadores.media(valores2);
+		
+		return correlacion - (media1 * media2);
+	}
+	
+	public static float coeficienteCorrelacionCruzada(int[] valores1, int[] valores2){
+		float covarianza = Indicadores.covarianzaCruzada(valores1, valores2);
+		float desvio1 = Indicadores.desviacionEstandar(valores1);
+		float desvio2 = Indicadores.desviacionEstandar(valores2);
+		
+		return covarianza / (desvio1 * desvio2);
+	}
 }
+
+
