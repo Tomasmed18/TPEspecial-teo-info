36089618640cf7c6a14d649209363a01c0c4f623
Tomasmed18 <tomy-medina@hotmail.com>
2018-06-13 00:02:31 -0300

    Agrego inciso 1

--- src/tp_especial/Main.java
+++ src/tp_especial/Main.java
@@ -18,6 +18,30 @@ public class Main {
 		ImagenEscalaGrises img8 = new ImagenEscalaGrises("resources/Will_Canal8.bmp");
 		ImagenEscalaGrises img10 = new ImagenEscalaGrises("resources/Will_Canal10.bmp");
 		
+		//---------Inciso 1
+		
+		String inciso1 = "";
+		
+		float[][] m2 = Probabilidades.matrizTransicion(img1, img2);
+		float[][] m8 = Probabilidades.matrizTransicion(img1, img8);
+		float[][] m10 = Probabilidades.matrizTransicion(img1, img10);
+		
+		inciso1 += "Matriz de transición del canal 2: " + String.format("%n");
+		for (int i = 0; i < 16; i++)
+			for (int j = 0; j < 16; j++)
+				inciso1 += "[" + i + "][" + j + "] = "  + m2[i][j] + String.format("%n");
+		
+		inciso1 += String.format("%n") + "Matriz de transición del canal 8: " + String.format("%n");
+		for (int i = 0; i < 16; i++)
+			for (int j = 0; j < 16; j++)
+				inciso1 += "[" + i + "][" + j + "] = "  + m8[i][j] + String.format("%n");
+		inciso1 += String.format("%n") + "Matriz de transición del canal 10: " + String.format("%n");
+		for (int i = 0; i < 16; i++)
+			for (int j = 0; j < 16; j++)
+				inciso1 += "[" + i + "][" + j + "] = "  + m10[i][j] + String.format("%n");
+		
+		System.out.println(inciso1);
+		
 		//---------Inciso 2
 		String inciso2 = "";
 		
@@ -57,17 +81,21 @@ public class Main {
 		System.out.println(inciso3);
 		
 		//---------Guardado en archivos
+		File p2inciso1 = new File("Parte2Inciso1.txt");
 		File p2inciso2 = new File("Parte2Inciso2.txt");
 		File p2inciso3 = new File("Parte2Inciso3.txt");
 		try {
-			FileOutputStream os1 = new FileOutputStream(p2inciso2);
-			FileOutputStream os2 = new FileOutputStream(p2inciso3);
+			FileOutputStream os1 = new FileOutputStream(p2inciso1);
+			FileOutputStream os2 = new FileOutputStream(p2inciso2);
+			FileOutputStream os3 = new FileOutputStream(p2inciso3);
 			
-			os1.write(inciso2.getBytes());
-			os2.write(inciso3.getBytes());
+			os1.write(inciso1.getBytes());
+			os2.write(inciso2.getBytes());
+			os3.write(inciso3.getBytes());
 			
 			os1.close();
 			os2.close();
+			os3.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
