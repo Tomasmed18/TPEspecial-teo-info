4aebdb4372fc3b560279e7e638860edacf2b9934
Tomasmed18 <tomy-medina@hotmail.com>
2018-06-12 23:07:03 -0300

    Add files via upload

--- src/tp_especial/Main.java
+++ src/tp_especial/Main.java
@@ -18,53 +18,53 @@ public class Main {
 		ImagenEscalaGrises img8 = new ImagenEscalaGrises("resources/Will_Canal8.bmp");
 		ImagenEscalaGrises img10 = new ImagenEscalaGrises("resources/Will_Canal10.bmp");
 		
-		//---------Inciso 1
-		String inciso1 = "";
-		
-		inciso1 += ("Ruido canal 2: " + Canales.ruido(img1, img2)) + String.format("%n");
-		inciso1 += ("Ruido canal 8: " + Canales.ruido(img1, img8)) + String.format("%n");
-		inciso1 += ("Ruido canal 10: " + Canales.ruido(img1, img10)) + String.format("%n");
-		
-		inciso1 += ("Pérdida canal 2: " + Canales.perdida(img1, img2)) + String.format("%n");
-		inciso1 += ("Pérdida canal 8: " + Canales.perdida(img1, img8)) + String.format("%n");
-		inciso1 += ("Pérdida canal 10: " + Canales.perdida(img1, img10)) + String.format("%n");
-		
-		inciso1 += ("Información Mutua canal 2: " + Canales.informacionMutua(img1, img2)) + String.format("%n");
-		inciso1 += ("Información Mutua 8: " + Canales.informacionMutua(img1, img8)) + String.format("%n");
-		inciso1 += ("Información Mutua canal 10: " + Canales.informacionMutua(img1, img10)) + String.format("%n");
-		
-		System.out.println(inciso1);
-		
-		
 		//---------Inciso 2
 		String inciso2 = "";
 		
-		int i = 10;
-		while (i <= 100000){
-			inciso2 += ("Ruido con " + i + " iteraciones: " + 
-					Canales.ruidoPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
-			inciso2 += ("Pérdida con " + i + " iteraciones: " + 
-					Canales.perdidaPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
-			inciso2 += ("Información Mutua con " + i + " iteraciones: " + 
-					Canales.informacionMutuaPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
-			i = i * 10;
-			inciso2 += String.format("%n");
-		}
-		inciso2 += ("Ruido por convergencia = " + Canales.ruidoPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
-		inciso2 += ("Pérdida por convergencia = " + Canales.perdidaPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
-		inciso2 += ("Información mutua por convergencia = " + Canales.informacionMutuaPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
+		inciso2 += ("Ruido canal 2: " + Canales.ruido(img1, img2)) + String.format("%n");
+		inciso2 += ("Ruido canal 8: " + Canales.ruido(img1, img8)) + String.format("%n");
+		inciso2 += ("Ruido canal 10: " + Canales.ruido(img1, img10)) + String.format("%n");
+		
+		inciso2 += ("Pérdida canal 2: " + Canales.perdida(img1, img2)) + String.format("%n");
+		inciso2 += ("Pérdida canal 8: " + Canales.perdida(img1, img8)) + String.format("%n");
+		inciso2 += ("Pérdida canal 10: " + Canales.perdida(img1, img10)) + String.format("%n");
+		
+		inciso2 += ("Información Mutua canal 2: " + Canales.informacionMutua(img1, img2)) + String.format("%n");
+		inciso2 += ("Información Mutua 8: " + Canales.informacionMutua(img1, img8)) + String.format("%n");
+		inciso2 += ("Información Mutua canal 10: " + Canales.informacionMutua(img1, img10)) + String.format("%n");
 		
 		System.out.println(inciso2);
 		
+		
+		//---------Inciso 3
+		String inciso3 = "";
+		
+		int i = 10;
+		while (i <= 100000){
+			inciso3 += ("Ruido con " + i + " iteraciones: " + 
+					Canales.ruidoPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
+			inciso3 += ("Pérdida con " + i + " iteraciones: " + 
+					Canales.perdidaPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
+			inciso3 += ("Información Mutua con " + i + " iteraciones: " + 
+					Canales.informacionMutuaPorSimulacion(new Fuente(img1), new Canal(img1, img2), i)) + String.format("%n");
+			i = i * 10;
+			inciso3 += String.format("%n");
+		}
+		inciso3 += ("Ruido por convergencia = " + Canales.ruidoPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
+		inciso3 += ("Pérdida por convergencia = " + Canales.perdidaPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
+		inciso3 += ("Información mutua por convergencia = " + Canales.informacionMutuaPorSimulacion(new Fuente(img1), new Canal(img1, img2))) + String.format("%n");
+		
+		System.out.println(inciso3);
+		
 		//---------Guardado en archivos
-		File p2inciso1 = new File("Parte2Inciso1.txt");
 		File p2inciso2 = new File("Parte2Inciso2.txt");
+		File p2inciso3 = new File("Parte2Inciso3.txt");
 		try {
-			FileOutputStream os1 = new FileOutputStream(p2inciso1);
-			FileOutputStream os2 = new FileOutputStream(p2inciso2);
+			FileOutputStream os1 = new FileOutputStream(p2inciso2);
+			FileOutputStream os2 = new FileOutputStream(p2inciso3);
 			
-			os1.write(inciso1.getBytes());
-			os2.write(inciso2.getBytes());
+			os1.write(inciso2.getBytes());
+			os2.write(inciso3.getBytes());
 			
 			os1.close();
 			os2.close();
